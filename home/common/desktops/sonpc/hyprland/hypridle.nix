15d724eff21ba6519c0445729f57721684ff3180
Son Roy Almerol <github@snry.me>
2024-09-08 22:42:41 -0400

    simplify hypridle config

--- home/common/desktops/sonpc/hyprland/hypridle.nix
+++ home/common/desktops/sonpc/hyprland/hypridle.nix
@@ -10,23 +10,10 @@
 
       listener = [
         {
-          timeout = 150;                               # 2.5min.
-          on-timeout = "brightnessctl -s set 10";         # set monitor backlight to minimum, avoid 0 on OLED monitor.
-          on-resume = "brightnessctl -r";                 # monitor backlight restore.
-        }
-        {
           timeout = 300;                                 # 5min
-          on-timeout = "loginctl lock-session";            # lock screen when timeout has passed
-        }
-        {
-          timeout = 330;                                 # 5.5min
-          on-timeout = "hyprctl dispatch dpms off";        # screen off when timeout has passed
+          on-timeout = "loginctl lock-session && hyprctl dispatch dpms off";            # lock screen when timeout has passed
           on-resume = "hyprctl dispatch dpms on";          # screen on when activity is detected after timeout has fired.
         }
-        #{
-        #    timeout = 1800                                # 30min
-        #    on-timeout = systemctl suspend                # suspend pc
-        #}
       ];
     };
   };
