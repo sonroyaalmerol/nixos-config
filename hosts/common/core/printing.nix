d1b27ede16f1cee4e6789000187955136399362d
Son Roy Almerol <github@snry.me>
2024-09-09 20:02:25 -0400

    add auto configure printers

--- hosts/common/core/printing.nix
+++ hosts/common/core/printing.nix
@@ -1,5 +1,14 @@
 {pkgs, ...}: {
   services.printing.enable = true;
+  services.printing.browsing = true;
+  services.printing.browsedConf = ''
+  BrowseDNSSDSubTypes _cups,_print
+  BrowseLocalProtocols all
+  BrowseRemoteProtocols all
+  CreateIPPPrinterQueues All
+
+  BrowseProtocols all
+  '';
   services.printing.drivers = with pkgs; [
     samsung-unified-linux-driver
   ];
