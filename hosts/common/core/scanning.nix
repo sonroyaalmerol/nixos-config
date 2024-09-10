88856a937c0af9f5acbe1d34e526729943533315
Son Roy Almerol <github@snry.me>
2024-09-09 20:30:03 -0400

    remove netconf probing

--- hosts/common/core/scanning.nix
+++ hosts/common/core/scanning.nix
@@ -1,7 +1,6 @@
 {pkgs, ...}: {
   hardware.sane.enable = true;
   hardware.sane.openFirewall = true;
-  hardware.sane.netConf = "192.168.11.104"; 
   hardware.sane.extraBackends = with pkgs; [
     sane-airscan
   ];
