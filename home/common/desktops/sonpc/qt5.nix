51422b53f0a3185e177c5335e4fc68d6fa14fa99
Son Roy Almerol <github@snry.me>
2024-10-24 23:18:16 -0400

    downgrade catppuccin

--- home/common/desktops/sonpc/qt5.nix
+++ home/common/desktops/sonpc/qt5.nix
@@ -1,7 +1,11 @@
 {pkgs, ...}: {
   qt = {
     enable = true;
+    platformTheme = {
+      name = "kvantum"; 
+    }; 
     style = {
+      name = "kvantum";
       catppuccin = {
         enable = true;
         apply = true;
