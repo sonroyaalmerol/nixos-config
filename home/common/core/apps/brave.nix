5729498a96152da4d48039e41002c250578a43ce
Son Roy Almerol <github@snry.me>
2024-12-02 17:32:17 -0500

    only use ozone platform brave

--- home/common/core/apps/brave.nix
+++ home/common/core/apps/brave.nix
@@ -2,9 +2,6 @@
   programs.brave = {
     enable = true;
     commandLineArgs = [
-      "--enable-features=VaapiVideoDecodeLinuxGL"
-      "--use-gl=angle"
-      "--use-angle=gl"
       "--ozone-platform=wayland"
     ];
   };
