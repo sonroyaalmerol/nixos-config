0eaa5a866c2f284f929fddc639dea6a38dee8ae1
Son Roy Almerol <github@snry.me>
2024-12-02 21:47:19 -0500

    try portalPackage

--- home/common/desktops/sonpc/hyprland/default.nix
+++ home/common/desktops/sonpc/hyprland/default.nix
@@ -4,6 +4,7 @@
   wayland.windowManager.hyprland = {
     enable = true;
     package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
   };
 
   home.file.".config/hypr" = {
